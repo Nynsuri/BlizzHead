466303ece0486eecf543ae9079f7461bcb685a61
Nynsuri <xylosen@gmail.com>
2024-03-31 20:41:19 +0200

    oddelenie header a footer

--- thankyou.php
+++ thankyou.php
@@ -9,24 +9,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-  <header class="container main-header">
-    <div>
-      <a href="index.html">
-        <img src="img/logo.jpg" height="65">
-      </a>
-    </div>
-  <nav class="main-nav">
-    <ul class="main-menu" id="main-menu">
-        <li><a href="index.html" class="blue">Domov</a></li>
-        <li><a href="wow.html" id="wow">World of Warcraft</a></li>
-        <li><a href="overwatch.html" id="ow">Overwatch</a></li>
-        <li><a href="kontakt.html" class="blue">Kontakt</a></li>
-    </ul>
-    <a class="hamburger" id="hamburger">
-        <i class="fa fa-bars"></i>
-    </a>
-  </nav>
-</header>
+<?php  
+  $file_path = "parts/header.php"; 
+  if(!include($file_path)) 
+  {     
+    echo"Failed to include $file_path";
+  } 
+  ?>
     
     <main>
       <!--kod sem-->
@@ -46,33 +35,13 @@
 
     </main>
     
-    <footer class="container bg-dark text-white">
-      <div class="row">
-        <div class="col-25">
-          <h4>Kto sme</h4>
-          <p>Toto je fanúšicka stranka určená len na informačne udaje.</p>
-          <p>Obrázky boli generované umelou inteligenciou</p>
-        </div>
-        <div class="col-25 text-left">
-          <h4>Kontaktujte nás</h4>
-          <p><i class="fa fa-envelope" aria-hidden="true"><a href="mailto:patrik.schveizer@gmail.com"> patrik.schveizer@gmail.com</a></i></p>
-          <p><i class="fa fa-phone" aria-hidden="true"><a href="tel:0909500600"> 0909500600</a></i></p>
-        </div>
-        <div class="col-25">
-          <h4>Rýchle odkazy</h4>
-          <p><a href="index.html">Domov</a></p>
-          <p><a href="kontakt.html">Kontakt</a></p>
-        </div>
-        <div class="col-25">
-          <h4>Nájdete nás</h4>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2654.4032594744617!2d18.061836792789137!3d48.295095197160926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476b3fabcd8fbdf3%3A0x84bb9c822558fe08!2zxL11ZG92w610YSDEjHVsw6FrYSA5MDUvMjAsIDk0OSAwMSBOaXRyYQ!5e0!3m2!1ssk!2ssk!4v1702565517611!5m2!1ssk!2ssk" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-         </div>
-      </div>
-      <div class="row">
-        Created and designed by Patrik <br>
-        Copyright ©
-      </div>
-    </footer>
+    <?php  
+  $file_path = "parts/footer.php"; 
+  if(!include($file_path)) 
+  {     
+    echo"Failed to include $file_path";
+  } 
+  ?>
 
     <script src="js/menu.js"></script>
 </body>
